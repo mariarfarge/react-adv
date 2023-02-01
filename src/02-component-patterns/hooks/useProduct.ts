fe39b675c93c547cd4c4f676f35735702b401e02
Rodriguez-Farge Hijano <maria.rodriguezfarge@sage.com>
2023-02-01 16:50:35 +0100

    fin del patron

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,34 +1,49 @@
-import { useEffect, useState } from 'react'
-import { onChangeArgs, Product } from '../interfaces/interfaces';
+import { useEffect, useRef, useState } from 'react'
+import { InitialValues, onChangeArgs, Product } from '../interfaces/interfaces';
 
 
 interface useProductArgs {
     product: Product;
     onChange?: ( args: onChangeArgs ) => void;
     value?: number;
+    initialValues?: InitialValues;
 }
 
 
-export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
+export const useProduct = ({ onChange, product, value = 0, initialValues }: useProductArgs) => {
 
-    const [ counter, setCounter ] = useState( value );
-
+    const [ counter, setCounter ] = useState<number>( initialValues?.count || value );
+    const isMounted = useRef(false);
 
     const increaseBy = ( value: number ) => {
-      
-        const newValue = Math.max( counter + value, 0 )
+        
+        const newValue = initialValues?.maxCount ? Math.min(Math.max( counter + value, 0 ), initialValues?.maxCount) : Math.max( counter + value, 0 )
+
         setCounter( newValue );
 
         onChange && onChange({ count: newValue, product });
     }
 
+    const reset = () => {
+        setCounter(initialValues?.count || value)
+    }
+
     useEffect(() => {
+        if ( !isMounted.current ) return;
         setCounter( value );
     }, [ value ])
 
+    useEffect(() => {
+        isMounted.current = true;
+    }, [])
+
     return {
         counter,
-        increaseBy
+        isMaxCountReached: !!initialValues?.maxCount && counter === initialValues.maxCount,
+        maxCount: initialValues?.maxCount,
+
+        increaseBy,
+        reset
     }
 
 }
